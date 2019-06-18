8a36553f252c90e923bb6b39b012cf2d2410a5d4
Kyle Kelly <kylekelly504@gmail.com>
2019-06-18 18:28:22 -0500

    added home component

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "", component: HomeComponent },
+    // { path: "", redirectTo: "/items", pathMatch: "full" },
+    // { path: "items", component: ItemsComponent },
+    // { path: "item/:id", component: ItemDetailComponent }
 ];
 
 @NgModule({
