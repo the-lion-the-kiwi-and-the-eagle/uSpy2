750eb819f592c68f4ca8ef1c633662ab0db12c81
Samual Moot <samualmoot@Samuals-MacBook-Pro-2.local>
2019-06-22 17:32:40 -0500

    small changes before pushing

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
-<<<<<<< HEAD
-import { ItemsComponent } from "./components/item/items.component";
-import { ItemDetailComponent } from "./components/item/item-detail.component";
-=======
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+
 import { LoginComponent } from "./components/login/login.component";
->>>>>>> cb05eeff2401cb05464de9d3c96af0fe158d7d6a
 import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-<<<<<<< HEAD
-import { NativeScriptHttpModule } from 'nativescript-angular/http'
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./components/item/items.component";
-import { ItemDetailComponent } from "./components/item/item-detail.component";
-=======
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives';
 import { HttpClientModule } from '@angular/common/http';
 import { NativeScriptUIDataFormModule } from 'nativescript-ui-dataform/angular/dataform-directives'
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
 import { LoginComponent } from "./components/login/login.component";
->>>>>>> cb05eeff2401cb05464de9d3c96af0fe158d7d6a
 import { HomeComponent } from "./components/home/home.component";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
@@ -36,24 +25,18 @@ import { Vision } from "./services/vision";
     ],
     imports: [
         NativeScriptModule,
-<<<<<<< HEAD
-        NativeScriptHttpModule,
-        AppRoutingModule
-=======
         NativeScriptUIDataFormModule,
         NativeScriptUISideDrawerModule,
         AppRoutingModule,
         HttpClientModule,
         ReactiveFormsModule,
         FormsModule,
->>>>>>> cb05eeff2401cb05464de9d3c96af0fe158d7d6a
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
         LoginComponent,
         HomeComponent,
-        ItemDetailComponent
+        
     ],
     providers: [
         Vision
