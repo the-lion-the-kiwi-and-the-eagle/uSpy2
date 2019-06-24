15bf7afdc178635c433edc5824e26346ebe666b2
Kyle Kelly <kylekelly504@gmail.com>
2019-06-24 14:48:51 -0500

    added profile component with correct routing

---
+++ my-angular-app/src/app/components/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
+
+@Component({
+  selector: 'ns-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private router: RouterExtensions) { }
+
+  ngOnInit() {
+  }
+
+  
+
+  backButton() {
+    this.router.navigate(['/list'])
+  }
+}
